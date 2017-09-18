9fcd4bba5969d8f70b00de7435ed67180672fcca
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-18 07:40:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,11 @@
 
 
 def turn_count(board)
-board.count {|player| player != " "}
-end
-
-
-def current_player(board)
+  turns_taken = 0
+  board.each do |occupied_position|
+    if occupied_position == "X" || occupied_position == "O"
+      turns_taken +=1
+    end
+  end
+  turns_taken
 end
